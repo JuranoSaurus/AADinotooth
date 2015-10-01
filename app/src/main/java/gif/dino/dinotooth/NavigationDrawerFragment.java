4d333c286680324d5020b9cf56069c22b5a22466
Jool <wnflrj123@gmail.com>
2015-10-02 03:04:50 +0900

    edit actionbar

--- app/src/main/java/gif/dino/dinotooth/NavigationDrawerFragment.java
+++ app/src/main/java/gif/dino/dinotooth/NavigationDrawerFragment.java
@@ -248,11 +248,6 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -263,7 +258,7 @@ public class NavigationDrawerFragment extends Fragment {
     private void showGlobalContextActionBar() {
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setTitle(R.string.app_name);
     }
 
