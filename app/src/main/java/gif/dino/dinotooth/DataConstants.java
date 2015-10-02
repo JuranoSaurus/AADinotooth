e484555d280ac6a09887cac88fb7c78d69b1a99f
Jool <wnflrj123@gmail.com>
2015-10-03 00:46:22 +0900

    add TCP connected~!

--- app/src/main/java/gif/dino/dinotooth/DataConstants.java
+++ app/src/main/java/gif/dino/dinotooth/DataConstants.java
@@ -6,4 +6,7 @@ package gif.dino.dinotooth;
 public class DataConstants {
 
     public static int DATA_MILES = 0;
+
+    public static String IP_ADDRESS = "192.168.42.1";
+    public static int PORT_NUM = 8888;
 }
--- app/src/main/java/gif/dino/dinotooth/LoadingActivity.java
+++ app/src/main/java/gif/dino/dinotooth/LoadingActivity.java
@@ -3,18 +3,64 @@ package gif.dino.dinotooth;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Fullscreen;
 
+import java.io.DataOutputStream;
+
 @Fullscreen
 @EActivity(R.layout.activity_loading)
 public class LoadingActivity extends Activity {
 
+    ToothSocket socket = null;
+    DataOutputStream dos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Handler mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message inputMessage) {
+                switch(inputMessage.what){
+                    case ToothSocket.MessageTypeClass.SIMSOCK_DATA :
+                        String msg = (String) inputMessage.obj;
+                        Log.d("JOOL", msg);
+                        Log.d("JOOL", "SIMSOCK_DATA ~~~~~~~~~! 심!솎!");
+                        Toast.makeText(getApplicationContext(), "SIMSOCK_DATA", Toast.LENGTH_LONG).show();
+                        // do something with UI
+                        break;
+
+                    case ToothSocket.MessageTypeClass.SIMSOCK_CONNECTED :
+                        // do something with UI
+                        Log.d("JOOL", "SIMSOCK_연결잼~~~~~~~~~! 심!솎!");
+                        Toast.makeText(getApplicationContext(), "SIMSOCK_CONNECTED", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case ToothSocket.MessageTypeClass.SIMSOCK_DISCONNECTED :
+                        // do something with UI
+                        Log.d("JOOL", "SIMSOCK_연결바이 ~~~~~~~~~! 심!솎!");
+                        Toast.makeText(getApplicationContext(), "SIMSOCK_DISCONNECTED", Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+            }
+        };
+
+        try {
+            socket = new ToothSocket(DataConstants.IP_ADDRESS, DataConstants.PORT_NUM, mHandler);
+            socket.start();
+        }catch (Exception e){
+            Log.e("JOOL", "socket error");
+        }
+
 
         // 1.5초후 시작
         doInBackgroundAfterTwoSeconds();
@@ -22,10 +68,12 @@ public class LoadingActivity extends Activity {
 
     @Background(delay = 1500)
     void doInBackgroundAfterTwoSeconds() {
-        Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
-        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(mainIntent);
+        if(socket != null) {
+            Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(mainIntent);
+        }
     }
 
 }
